0a484ce6a0a60b3b8ca3431a54d2904ae90e86c6
Marc Brugger <github@bakito.ch>
2025-07-21 09:47:29 +0200

    Update server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -127,15 +127,15 @@ func handleTouch(cl k8s.Client, res config.Resource) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		namespace := c.Param("namespace")
 		name := c.Param("name")
-		
+
 		l := slog.WithValues("resource", res.Name, "namespace", namespace, "name", name)
-		
+
 		value := metav1.Now().Format(time.RFC3339)
 		if user:= c.GetHeader(headerArgoCDUsername); user != "" {
 			value += " by: " + user
 			l = l.WithValues("user", user)
 		}
-		
+
 		if err := cl.PatchAnnotation(c, res, namespace, name, "argocd.bakito.ch/touch", value); err != nil {
 			l.Error("Failed to touch resource", "error", err)
 			var se *kerr.StatusError
