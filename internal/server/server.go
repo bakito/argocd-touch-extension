916108fd795df0f916f855c9288a33011a8eda58
Marc Brugger <github@bakito.ch>
2025-07-21 09:54:36 +0200

    Update server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -131,7 +131,7 @@ func handleTouch(cl k8s.Client, res config.Resource) gin.HandlerFunc {
 		l := slog.With("resource", res.Name, "namespace", namespace, "name", name)
 
 		value := metav1.Now().Format(time.RFC3339)
-		if user:= c.GetHeader(headerArgoCDUsername); user != "" {
+		if user := c.GetHeader(headerArgoCDUsername); user != "" {
 			value += " by: " + user
 			l = l.With("user", user)
 		}
