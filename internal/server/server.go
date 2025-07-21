a20bc57f09ff1f6d8bca35ee0bc083a2db9f5bf2
Marc Brugger <github@bakito.ch>
2025-07-21 09:56:50 +0200

    Add user if available (#4)

--- internal/server/server.go
+++ internal/server/server.go
@@ -25,7 +25,7 @@ const (
 	headerArgocdAppName       = "Argocd-Application-Name"
 	headerArgocdProjName      = "Argocd-Project-Name"
 	headerArgocdExtensionName = "Argocd-Touch-Extension-Name"
-	// headerArgoCDUsername    = "Argocd-Username" // .
+	headerArgoCDUsername      = "Argocd-Username"
 	// headerArgoCDGroups      = "Argocd-User-Groups" // .
 
 	touchAPIPath = "/v1/touch"
@@ -128,8 +128,16 @@ func handleTouch(cl k8s.Client, res config.Resource) gin.HandlerFunc {
 		namespace := c.Param("namespace")
 		name := c.Param("name")
 
-		if err := cl.PatchAnnotation(c, res, namespace, name, "argocd.bakito.ch/touch", metav1.Now().Format(time.RFC3339)); err != nil {
-			slog.Error("Failed to patch annotation", "error", err, "resource", res.Name, "namespace", namespace, "name", name)
+		l := slog.With("resource", res.Name, "namespace", namespace, "name", name)
+
+		value := metav1.Now().Format(time.RFC3339)
+		if user := c.GetHeader(headerArgoCDUsername); user != "" {
+			value += " by: " + user
+			l = l.With("user", user)
+		}
+
+		if err := cl.PatchAnnotation(c, res, namespace, name, "argocd.bakito.ch/touch", value); err != nil {
+			l.Error("Failed to touch resource", "error", err)
 			var se *kerr.StatusError
 			if errors.As(err, &se) {
 				c.JSON(int(se.Status().Code), err)
@@ -138,6 +146,7 @@ func handleTouch(cl k8s.Client, res config.Resource) gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, err)
 			return
 		}
+		l.Info("Ressource touched")
 
 		c.Status(http.StatusOK)
 	}
