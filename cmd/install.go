b1f1847c608c229624815fdfb7566e18ab5ebb4a
Marc Brugger <github@bakito.ch>
2025-08-18 11:44:10 +0200

    feat: enable graceful error handling for install function (#23)

--- cmd/install.go
+++ cmd/install.go
@@ -1,18 +1,29 @@
 package cmd
 
 import (
+	"log/slog"
+
 	"github.com/bakito/argocd-touch-extension/internal/install"
 	"github.com/spf13/cobra"
 )
 
-var installCmd = &cobra.Command{
-	Use:   "install",
-	Short: "Install the UI extension to argocd server",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return install.Do(cmd.Context())
-	},
-}
+var (
+	grace      bool
+	installCmd = &cobra.Command{
+		Use:   "install",
+		Short: "Install the UI extension to argocd server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := install.Do(cmd.Context())
+			if err != nil && !grace {
+				return err
+			}
+			slog.ErrorContext(cmd.Context(), "Extension installation failed, but continuing", "error", err)
+			return nil
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(installCmd)
+	installCmd.Flags().BoolVarP(&grace, "graceful", "g", false, "Continues normally if there is an error")
 }
